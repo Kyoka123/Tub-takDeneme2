5027171027f7e88642b63e5a234f535e3477c529
Kyoka123 <ergundkgu2314@gmail.com>
2026-03-26 00:59:24 +0300

    Question pop up için panel bitti
    soru yazmak için arka plan hazır,, aşağı yukarı inmesini ayarladım süreleri ve hareket etme hızı ayarlanabilir.

---
+++ Assets/Scripts/QuestionShow.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using UnityEngine;
+
+public class QuestionShow : MonoBehaviour
+{
+    public RectTransform panel;
+    public float moveSpeed = 800f;
+    public float showTime = 3f;
+    public float waitTime = 2f;
+
+    private float hiddenY = 200f;
+    private float visibleY = 100f;
+
+    void Start()
+    {
+        panel.anchoredPosition = new Vector2(panel.anchoredPosition.x, hiddenY);
+        StartCoroutine(PanelLoop());
+    }
+
+    IEnumerator PanelLoop()
+    {
+        while (true)
+        {
+            // Aþaðý in
+            yield return MovePanel(visibleY);
+            Debug.Log("Soru gosteriliyor");
+
+            yield return new WaitForSeconds(showTime);
+
+            //Yukarý çýk
+            yield return MovePanel(hiddenY);
+
+            yield return new WaitForSeconds(waitTime);
+
+            Debug.Log("Yeni soru hazýrlanýyor");
+        }
+    }
+
+    IEnumerator MovePanel(float targetY)
+    {
+        while (Mathf.Abs(panel.anchoredPosition.y - targetY) > 1f)
+        {
+            Vector2 pos = panel.anchoredPosition;
+            pos.y = Mathf.MoveTowards(pos.y, targetY, moveSpeed * Time.deltaTime);
+            panel.anchoredPosition = pos;
+            yield return null;
+        }
+    }
+}
