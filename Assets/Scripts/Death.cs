afd0980597bd4c184d605d4758716209d2b61155
Kyoka123 <ergundkgu2314@gmail.com>
2026-05-27 01:28:59 +0300

    deaaatthhhh

--- Assets/Scripts/Death.cs
+++ Assets/Scripts/Death.cs
@@ -1,22 +1,37 @@
+using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 
 public class Death : MonoBehaviour
 {
     Vector3 Targetpoint;
-   
-    
-    private void OnTriggerEnter(Collider deniz)
+    Vector3 Targetpoint2;
+    // Trigger enter alýyoruz ve playerýn tagine göre hareket ediyoruz
+    // Trigger almamýzýn sebebi eðer collision alýrsak her collisionda olurdu
+    private void OnTriggerEnter(Collider araba)
     {
-        Targetpoint = new Vector3(Random.Range(0, 10), 2, Random.Range(5, 15));
-        if (deniz.gameObject.tag == "Player")
+        do
+        {
+           Targetpoint = new Vector3(Random.Range(-20, 18), 2, Random.Range(2, 40));
+           Targetpoint2 = new Vector3(Random.Range(-20, 18), 2, Random.Range(2, 40));
+        } while (Vector3.Distance(Targetpoint, Targetpoint2) < 3); // Ensure that the two target points are not the same
+
+
+
+
+
+        if (araba.gameObject.tag == "Player")
         {
            GameObject.FindGameObjectWithTag("Player").transform.rotation = Quaternion.Euler(0,0,0);
            GameObject.FindGameObjectWithTag("Player").transform.position = Targetpoint;
+           GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody>().linearVelocity = Vector3.zero;
+           GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
         }
-        if (deniz.gameObject.tag == "Player2")
+        if (araba.gameObject.tag == "Player2")
         {
            GameObject.FindGameObjectWithTag("Player2").transform.rotation = Quaternion.Euler(0, 0, 0);
-           GameObject.FindGameObjectWithTag("Player2").transform.position = Targetpoint;
+           GameObject.FindGameObjectWithTag("Player2").transform.position = Targetpoint2;
+           GameObject.FindGameObjectWithTag("Player2").GetComponent<Rigidbody>().linearVelocity = Vector3.zero;
+           GameObject.FindGameObjectWithTag("Player2").GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
         }
     }
 }
